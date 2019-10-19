f3ec4cc173e2fb846f6882b809729b54694008ef
nkflores <nkflores@hotmail.com>
2019-10-19 14:36:20 -0600

    updated assets and html

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+
+var targetScores;
+var playerScore=0;
+var gem1=Math.floor(Math.random() *12) +1;
+var gem2=0;
+var gem3=0;
+var gem4=0;
+var wins=0;
+var losses=0;
+
+function startGame() {
+    targetScore = Math.floor(Math.random() * 102) + 19;
+    $('#target-score').text(targetScore);
+
+    $("#gem-1").click(function() + "gem1");
+        
+    })
+     
+    
+
+}
+
+
+
+
+startGame();
+
+
+
+
+
+
+
+
